733298dc856f69389494801458d301901445bfea
sunfeilong <sunfeilong1993@gmail.com>
2019-12-11 22:22:57 +0800

    regex

---
+++ process/src/demo/regex_demo.go
@@ -0,0 +1,21 @@
+package main
+
+import "regexp"
+
+import "fmt"
+
+import "bytes"
+
+func main() {
+	match, _ := regexp.MatchString("p([a-z]+)ch", "peach")
+	fmt.Println("match: ", match)
+
+	r := regexp.MustCompile("p([a-z]+)ch")
+	fmt.Println("peach: ", r.MatchString("peach"))
+	fmt.Println("peaach: ", r.MatchString("peaach"))
+	fmt.Println("peaachch  peaadasdaadasdasdh: ", r.FindString("peaachch  peaadasdch paadaschdasdh"))
+
+	in := []byte("a peach a peachchchchhchch")
+	out := r.ReplaceAllFunc(in, bytes.ToUpper)
+	fmt.Println(string(out))
+}
