36509c5682a2edc8f3b30a2fb56b1786152a3e5d
sunfeilong <sunfeilong1993@gmail.com>
2019-12-21 23:50:49 +0800

    code

---
+++ process/src/demo/byte_buffer_demo.go
@@ -0,0 +1,43 @@
+package main
+
+import "bytes"
+
+import "fmt"
+
+func main() {
+	fmt.Println(intsToString([]int{1, 2, 3, 4, 5, 6, 7}))
+	fmt.Println(comm("123"))
+	fmt.Println(comm("123456789"))
+}
+
+func intsToString(numbers []int) string {
+
+	var buf bytes.Buffer
+	buf.WriteByte('[')
+	for i, v := range numbers {
+		if i > 0 {
+			buf.WriteString(",")
+		}
+		fmt.Fprintf(&buf, "%d", v)
+	}
+	buf.WriteByte(']')
+	return buf.String()
+}
+
+func comm(number string) string {
+	l := len(number)
+	if l <= 3 {
+		return number
+	}
+
+	var buf bytes.Buffer
+	split := ""
+	for i := 0; i < l; {
+		buf.WriteString(split)
+		buf.WriteString(number[i : i+3])
+		split = ","
+		i += 3
+	}
+
+	return buf.String()
+}
