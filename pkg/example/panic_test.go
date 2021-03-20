d7b53fd11d7fcf39369ff971041b5c6790227644
sunfeilong <sunfeilong1993@gmail.com>
2021-03-20 22:39:29 +0800

    Tes

--- pkg/example/panic_test.go
+++ pkg/example/panic_test.go
@@ -1,11 +1,18 @@
 package demo
 
 import (
+	"fmt"
 	"os"
 	"testing"
 )
 
 func TestPanic(t *testing.T) {
+
+	defer func() {
+		if r := recover(); nil != r {
+			fmt.Println("recover")
+		}
+	}()
 
 	_, err := os.Create("/tmp/file")
 
