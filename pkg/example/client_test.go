b46ef557f58c202170c85d2bb6dac331b5e6e6ef
sunfeilong <sunfeilong1993@gmail.com>
2021-04-23 11:41:09 +0800

    udp client and server

--- pkg/example/client_test.go
+++ pkg/example/client_test.go
@@ -1,24 +1,60 @@
 package demo
 
 import (
-	"bufio"
 	"fmt"
 	"log"
 	"net"
+	"strconv"
+	"strings"
 	"testing"
 )
 
 func TestClient(t *testing.T) {
-	conn, err := net.Dial("tcp", ":8686")
+	udpConnect, err := net.DialUDP("udp", nil, &net.UDPAddr{
+		IP:   net.IPv4(127, 0, 0, 1),
+		Port: 8686,
+	})
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer conn.Close()
-	fmt.Fprintf(conn, "hello\n")
-	res, err := bufio.NewReader(conn).ReadString('\n')
+	_, err = udpConnect.Write([]byte("hello i am client"))
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(string(res))
-	fmt.Fprintf(conn, "Jimmy!\n")
+
+	bytes := make([]byte, 100)
+	read, _ := udpConnect.Read(bytes)
+
+	ipAndPort := string(bytes[0:read])
+	log.Println("ipAndPort: ", ipAndPort)
+	listen(ipAndPort)
+}
+
+func listen(ipAndPort string) {
+	split := strings.Split(ipAndPort, ":")
+
+	ipStr := strings.Split(split[0], ".")
+	ip1, _ := strconv.Atoi(ipStr[0])
+	ip2, _ := strconv.Atoi(ipStr[1])
+	ip3, _ := strconv.Atoi(ipStr[2])
+	ip4, _ := strconv.Atoi(ipStr[3])
+
+	port, _ := strconv.Atoi(split[1])
+
+	udpConnect, err := net.ListenUDP("udp", &net.UDPAddr{
+		IP:   net.IPv4(byte(ip1), byte(ip2), byte(ip3), byte(ip4)),
+		Port: port,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		bytes := make([]byte, 100)
+		_, remoteAddr, err := udpConnect.ReadFromUDP(bytes)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		log.Println("revive data: ", string(bytes), "remoteAddr:", remoteAddr)
+	}
 }
--- pkg/example/server_test.go
+++ pkg/example/server_test.go
@@ -5,26 +5,41 @@ import (
 	"log"
 	"net"
 	"testing"
+	"time"
 )
 
 func TestHttpServer(t *testing.T) {
-	l, err := net.Listen("tcp", ":8686")
+	udpConnect, err := net.ListenUDP("udp", &net.UDPAddr{
+		IP:   net.IPv4(127, 0, 0, 1),
+		Port: 8686,
+	})
 	if err != nil {
 		log.Fatal(err)
 	}
 	for {
-		conn, err := l.Accept()
+		bytes := make([]byte, 100)
+		_, remoteAddr, err := udpConnect.ReadFromUDP(bytes)
+		fmt.Println("revive data: ", string(bytes))
 		if err != nil {
-			log.Fatal(err)
+			fmt.Println(err)
+			continue
 		}
-		go handleConnection(conn)
-	}
-}
-
-func handleConnection(conn net.Conn) {
-	fmt.Println("remote addr: ", conn.RemoteAddr())
-	err := conn.Close()
-	if err != nil {
-		fmt.Println(err)
+		fmt.Println("remote addr: ", remoteAddr.String())
+		_, err = udpConnect.WriteToUDP([]byte(remoteAddr.String()), remoteAddr)
+		if err != nil {
+			fmt.Println(err)
+		}
+		time.Sleep(time.Second)
+		//
+		go func(remote *net.UDPAddr) {
+			connect, err := net.DialUDP("udp4", nil, remote)
+			if err != nil {
+				log.Fatal(err)
+			}
+			_, err = connect.Write([]byte("hello i am server"))
+			if err != nil {
+				log.Fatal(err)
+			}
+		}(remoteAddr)
 	}
 }
